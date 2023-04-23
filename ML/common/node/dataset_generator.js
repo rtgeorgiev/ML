b9ecc9547af99df5054961bd61e64fd17a4854f0
rtgeorgiev <rgeorgiev001@gmail.com>
2023-04-23 11:08:58 +0100

    Data viewer, dataset generator, feature extractor

--- ML/common/node/dataset_generator.js
+++ ML/common/node/dataset_generator.js
@@ -1,4 +1,4 @@
-const draw=require('../common/draw.js');
+const draw = require('../common/draw.js');
 const constants=require('../common/constants.js');
 const utils=require('../common/utils.js');
 
@@ -8,6 +8,7 @@ const ctx=canvas.getContext('2d');
 
 const fs=require('fs');
 
+// This extra section helps to install the package (was not in the tutorial)
 if(fs.existsSync(constants.DATASET_DIR)){
    fs.readdirSync(constants.DATASET_DIR).forEach(fileName => 
       fs.rmSync(constants.DATASET_DIR+"/"+fileName,{recursive:true}
@@ -18,6 +19,7 @@ fs.mkdirSync(constants.DATASET_DIR);
 fs.mkdirSync(constants.JSON_DIR);
 fs.mkdirSync(constants.IMG_DIR);
 console.log("GENERATING DATASET ...");
+// End of extra section
 
 const fileNames=fs.readdirSync(constants.RAW_DIR);
 const samples=[];
